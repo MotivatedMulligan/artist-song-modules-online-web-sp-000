26b87d326b5de9e0f1a4fb485eceeef94380a76f
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-05-01 19:38:40 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,10 @@
  module Findable
 
-def self.find_by_name(name)
-  self.all.detect{|a|a.name == name}
-end
+# def self.find_by_name(name)
+#   self.all.detect{|a|a.name == name}
+# end
 
+def find_by_name(name)
+  all.detect{|a|a.name == name}
+end
 end
