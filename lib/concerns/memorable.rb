08a52411b787f1880af95597b301a97506d2f170
Sean Mulligan <sean.paul.mulligan@gmail.com>
2021-05-01 19:30:03 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,14 +1,17 @@
 module Memorable
-
+module InstanceMethods
  def initialize
-
-
- end
+   
+end
+end
+module ClassMethods
+ 
  def reset_all
   all.clear
 end
 
 def count
   all.count
+end
 end
 end
